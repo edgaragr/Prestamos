8344e4cb25b23559d4c2364039a1c9ee2a2718ac
Edgar Garcia <edgar.garcia@gmail.com>
2011-06-02 14:58:09 -0400

    Custom Exception

---
+++ src/main/java/com/wiled/ubicame/prestamo/utils/PrestamoException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wiled.ubicame.prestamo.utils;
+
+/**
+ *
+ * @author Edgar Garcia
+ */
+public class PrestamoException extends Exception {
+    public PrestamoException(String message) {
+        super(message);
+    }    
+}
