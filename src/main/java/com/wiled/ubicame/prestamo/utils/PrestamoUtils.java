86813cdb8bc3d34bba819a6703b337b1ac3c6750
Edgar Garcia <edgar.garcia@gmail.com>
2011-06-08 09:11:56 -0400

    Se agregaron metodos de validacion de cedula y telefono

--- src/main/java/com/wiled/ubicame/prestamo/utils/PrestamoUtils.java
+++ src/main/java/com/wiled/ubicame/prestamo/utils/PrestamoUtils.java
@@ -12,4 +12,29 @@ public class PrestamoUtils {
     public static float amortizarPrestamo(double monto, float tasa) {
         return 0f;
     }
+    
+    public static boolean containsOnlyNumbers(String str) {
+        if (str == null || str.length() == 0)
+            return false;
+        
+        //Replace '-'
+        str = str.replaceAll("-", "");
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i)))
+                return false;
+        }
+        
+        return true;
+    }
+    
+    public static  boolean isCedulaSizeValid(String cedula) {
+        if(cedula.length() == 11) return true;
+        return false;
+    }
+    
+    public static boolean isTelefonoSizeValid(String telefono) {
+        if(telefono.length() == 10) return true;
+        return false;
+    }
 }
