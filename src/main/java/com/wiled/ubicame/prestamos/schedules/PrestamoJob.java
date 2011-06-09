1da6bf936fb46fd8894dd4cbcafddd8e7423982d
Edgar Garcia <edgar.garcia@gmail.com>
2011-06-09 13:26:47 -0400

    Se agrego informacion de logs

--- src/main/java/com/wiled/ubicame/prestamos/schedules/PrestamoJob.java
+++ src/main/java/com/wiled/ubicame/prestamos/schedules/PrestamoJob.java
@@ -8,13 +8,13 @@ import com.wiled.ubicame.prestamo.utils.PrestamoConstants;
 import com.wiled.ubicame.prestamo.utils.PrestamoException;
 import com.wiled.ubicame.prestamos.datalayer.Controller;
 import com.wiled.ubicame.prestamos.entidades.Prestamo;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -22,16 +22,21 @@ import org.quartz.JobExecutionException;
  */
 public class PrestamoJob implements Job {
     public static String PRESTAMO = "prestamo";
+    private static final Logger log = LoggerFactory.getLogger(PrestamoJob.class);
     
     @Override
     public void execute(JobExecutionContext jec) throws JobExecutionException {
+        
         JobDetail jobDetail = jec.getJobDetail();
         
         JobDataMap map = jobDetail.getJobDataMap();
-        int prestamoId = map.getInt(PRESTAMO);
+        Long prestamoId = map.getLong(PRESTAMO);
         
         Controller database = Controller.getInstance(PrestamoConstants.PROD_PU);
         Prestamo prestamo = database.buscarPrestamo(prestamoId);
+                
+        log.info("**********************Aplicando intereses al prestamo: " + prestamo.getId());
+        
         
         //calcular cuota
         double cuota = 0;
@@ -39,8 +44,10 @@ public class PrestamoJob implements Job {
         try {            
             cuota = database.amortizarPrestamo(prestamo.getMonto(), prestamo.getTasa());
         } catch (PrestamoException ex) {
-            Logger.getLogger(PrestamoJob.class.getName()).log(Level.SEVERE, null, ex);
+            log.error(ex.getMessage(), ex);
         }
+        
+        log.info("***********************   Cuota aplicada: " + cuota);
         
         double interesesAcumulados = prestamo.getInteresAcumulado();        
         prestamo.setInteresAcumulado(interesesAcumulados + cuota);
