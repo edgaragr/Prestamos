345a1a4d400bd9ef28bd1b763672783d26bdd00d
Edgar Garcia <edgar.garcia@gmail.com>
2011-07-21 18:47:23 -0400

    Modificacion para permitir creacion de clientes sin cedula ni telefono

--- src/main/java/com/wiled/ubicame/prestamos/forms/CrearClientes.java
+++ src/main/java/com/wiled/ubicame/prestamos/forms/CrearClientes.java
@@ -185,14 +185,14 @@ public class CrearClientes extends javax.swing.JDialog {
             return;
         }
         
-        if(cedulaTxt.getText().isEmpty() || !containsOnlyNumbers(cedulaTxt.getText()) || !isCedulaSizeValid(cedulaTxt.getText())) {
+        if(!cedulaTxt.getText().isEmpty() && (!containsOnlyNumbers(cedulaTxt.getText()) || !isCedulaSizeValid(cedulaTxt.getText()))) {
             JOptionPane.showMessageDialog(rootPane, "Por favor introduzca una cedula valida", "ERROR", JOptionPane.ERROR_MESSAGE);
             cedulaTxt.grabFocus();
             return;            
         }
         
-        if(telefonoTxt.getText().isEmpty() || !containsOnlyNumbers(telefonoTxt.getText()) || !isTelefonoSizeValid(telefonoTxt.getText())) {
-            JOptionPane.showMessageDialog(rootPane, "Por favor introduzca un telefono valida", "ERROR", JOptionPane.ERROR_MESSAGE);
+        if(!telefonoTxt.getText().isEmpty() && (!containsOnlyNumbers(telefonoTxt.getText()) || !isTelefonoSizeValid(telefonoTxt.getText()))) {
+            JOptionPane.showMessageDialog(rootPane, "Por favor introduzca un telefono valido", "ERROR", JOptionPane.ERROR_MESSAGE);
             telefonoTxt.grabFocus();
             return;            
         }
